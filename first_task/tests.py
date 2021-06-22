607d40a9b46390651bb9c10c0b75bb7842811592
Polina Supranovich <p.supranovich@gmail.com>
2021-06-22 21:01:02 +0300

    new

--- first_task/tests.py
+++ first_task/tests.py
@@ -16,7 +16,7 @@ class Setup(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        self.driver = webdriver.Chrome(executable_path='../chromedriver')
+        self.driver = webdriver.Chrome('chromedriver.exe')
         self.driver.implicitly_wait(10)
 
     def tearDown(self) -> None:
