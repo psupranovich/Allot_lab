2e38cbe7640c7d5229e964ef48069ebad11928c1
Polina Supranovich <p.supranovich@gmail.com>
2021-06-22 19:51:50 +0300

    script_problem

--- first_task/tests.py
+++ first_task/tests.py
@@ -16,18 +16,20 @@ class Setup(unittest.TestCase):
 
     def setUp(self) -> None:
 
-        capabilities = {
-            "browserName": "chrome",
-            "browserVersion": "91.0",
-            "selenoid:options": {
-                "enableVNC": True,
-                "enableVideo": False
-            }
-        }
+        self.driver = webdriver.Chrome()
 
-        self.driver = webdriver.Remote(
-            command_executor="http://localhost:4444/wd/hub",
-            desired_capabilities=capabilities)
+        capabilities = {
+        #     "browserName": "chrome",
+        #     "browserVersion": "91.0",
+        #     "selenoid:options": {
+        #         "enableVNC": True,
+        #         "enableVideo": False
+        #     }
+        # }
+        #
+        # self.driver = webdriver.Remote(
+        #     command_executor="http://localhost:4444/wd/hub",
+        #     desired_capabilities=capabilities)
 
         self.driver.implicitly_wait(10)
 
